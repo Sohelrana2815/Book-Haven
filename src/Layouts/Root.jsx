32040fc8f42fe1702eca0341f89b66dae6c3bc0e
Sohelrana2815 <sohel152302@gmail.com>
2024-07-29 10:33:54 +0600

    navbar and footer from daisy ui

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Shared/Navbar/Navbar";
+import Footer from "../Components/Shared/Footer/Footer";
+
+const Root = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Root;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      maxWidth: {
+        "screen-3xl": "1660px",
+      },
       fontFamily: {
         playfair: ['"Playfair Display"', "serif"],
         worksans: ['"Work Sans"', "sans-serif"],
